b8b6470866c3fbd05679e359d4e01286f15b1a11
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-06 11:50:03 -0700

    Add error handling to WaterUsage routes

--- routes.js
+++ routes.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const WaterUsage = require('./database');
 
 router.post('/record', async (req, res) => {
-    const { user, amount } = req.body;
-    const newUsage = new WaterUsage({ user, amount });
-    await newUsage.save();
-    res.status(201).send(newUsage);
+    try {
+        const { user, amount } = req.body;
+        const newUsage = new WaterUsage({ user, amount });
+        await newUsage.save();
+        res.status(201).send(newUsage);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to record water usage' });
+    }
 });
 
 router.get('/usages', async (req, res) => {
-    const usages = await WaterUsage.find();
-    res.send(usages);
+    try {
+        const usages = await WaterUsage.find();
+        res.send(usages);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to retrieve usages' });
+    }
 });
 
 module.exports = router;
